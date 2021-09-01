18c4e4307ec8b769be83733dc7ee7ce668173fb1
Hannah Culver <hc219417@ohio.edu>
2021-09-01 18:34:21 -0400

    hello there

---
+++ git-lab-program.cc
@@ -0,0 +1,17 @@
+/*
+*   File: git-lab-programm.cc
+*   Author: Hannah Culver
+*   Date: September 1, 2021
+*   Description: c++ program in git-lab-2 repository
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main (int argc, char const *argv [])
+{
+    cout << "Hello Git!!" << endl;
+    return 0;
+}
